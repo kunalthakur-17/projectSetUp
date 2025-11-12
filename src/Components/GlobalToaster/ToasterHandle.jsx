e4cf2b923dc77e95220644505719f55904cbdaea
Kunal Thakur <141919988+kunalthakur-17@users.noreply.github.com>
2025-11-12 13:06:33 +0530

    first commit

--- src/Components/GlobalToaster/ToasterHandle.jsx
+++ src/Components/GlobalToaster/ToasterHandle.jsx
@@ -8,24 +8,11 @@ export default function GlobalToaster({
   message,
   severity,
 }) {
-  //   const [open, setOpen] = useState(false);
-  //   const [message, setMessage] = useState("");
-  //   const [severity, setSeverity] = useState("info"); // success, error, warning, info
 
-  // Function to show toaster message
-  //   const showToast = (msg, sev = "info") => {
-  //     setMessage(msg);
-  //     setSeverity(sev);
-  //     setOpen(true);
-  //   };
-
-  // Example usage: call showToast('Hello world!', 'success') anywhere to show
 
   return (
     <>
-      {/* Add your app components here */}
-
-      {/* Snackbar toaster */}
+    
       <Snackbar
         open={open}
         autoHideDuration={3000}
